c207d596ff52e3ced02cdfa959965782ac6302f2
pichetswn <pichetswn@gmail.com>
2018-11-13 11:30:37 +0700

    Standart Flutter

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Hello Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Hello Flutter AppBar'),
+        ),
+        body: new Text('This is Body'),
+      ),
+    );
+  }
+}
